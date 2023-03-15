c6787434fe79f62fa9ed9c9200204762a58ceb1d
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-03-15 21:34:13 +0330

    add todos

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
-import TodoForm from './TodoForm'
-import TodoList from './TodoList'
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 const TodoApp = () => {
-    const [todos, setTodos] = useState([])
-  return (
-    <div className='container'>
-        <TodoForm />
-        <TodoList/>
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
 
-export default TodoApp
+  const addTodoHandler = (input) => {
+    // console.log(input)
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+      isCompleted: false,
+    };
+    setTodos([...todos, newTodo])
+  };
+  return (
+    <div className="container">
+      <TodoForm addTodoHandler={addTodoHandler} />
+      <TodoList />
+    </div>
+  );
+};
+
+export default TodoApp;
